7f50d04eb8ddb004fee40037084232dc6a438787
niuqh <371464685@qq.com>
2016-04-19 20:37:29 +0800

    用Intent中activity中传输数据

--- app/src/main/java/com/example/niuqihong/activitytest/MainActivity.java
+++ app/src/main/java/com/example/niuqihong/activitytest/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 //在intent之间传输数据
                 String stringData = "use Intent Activity pass data";
                 Intent intent = new Intent(MainActivity.this,ThirdActivity.class);
-                intent.putExtra("extar_date",stringData);
+                intent.putExtra("extar_data",stringData);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/niuqihong/activitytest/ThirdActivity.java
+++ app/src/main/java/com/example/niuqihong/activitytest/ThirdActivity.java
@@ -3,8 +3,10 @@ package com.example.niuqihong.activitytest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -12,7 +14,11 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
-
+        //获取intent 之间传输的数据
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extar_data");
+        Toast.makeText(ThirdActivity.this,data,Toast.LENGTH_SHORT).show();
+//        Log.d(TAG, "onCreate:data ");
 
 
     }
